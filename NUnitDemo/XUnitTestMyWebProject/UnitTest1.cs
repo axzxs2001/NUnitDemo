7f13338a442982ae39ef6a42e05d364aaa2dbb81
桂素伟 <axzxs2001@163.com>
2017-07-25 11:17:18 +0800

    XUnit添加多用例和异常处理。

--- NUnitDemo/XUnitTestMyWebProject/UnitTest1.cs
+++ NUnitDemo/XUnitTestMyWebProject/UnitTest1.cs
@@ -5,12 +5,33 @@ namespace XUnitTestMyWebProject
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(10, 3, 7)]
+        [InlineData(20, 11, 9)]
+        public void Test1(int result, int a, int b)
         {
-            Assert.Equal(10, add(4, 6));
+            Assert.Equal(result, add(a, b));
+        }
+        [Fact]
+        public void Test2()
+        {
+            Assert.NotEqual(10, add(2, 3));
+        }
+        /// <summary>
+        /// 异常处理
+        /// </summary>
+        [Fact]       
+        public void Test3()
+        {
+            var exc = Assert.Throws<DivideByZeroException>(() => abc());
+          
+            Assert.Contains("被除数为", exc.Message);
         }
 
+        void abc()
+        {
+            throw new DivideByZeroException("被除数为0");
+        }
         int add(int a, int b)
         {
             return a + b;
