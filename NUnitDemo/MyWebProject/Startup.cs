6e37d9df6d0f05b60b021172c32c7fcfa46b5104
桂素伟 <axzxs2001@163.com>
2017-08-15 10:15:05 +0800

    迁移到.net core 2.0 迁移到asp.net core 2.0

--- NUnitDemo/MyWebProject/Startup.cs
+++ NUnitDemo/MyWebProject/Startup.cs
@@ -18,23 +18,12 @@ namespace MyWebProject
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-
-
-            Mapper.Initialize(cfg =>
-            {
-                cfg.CreateMap<DataModel.Drug, Drug>();     
-            });
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
